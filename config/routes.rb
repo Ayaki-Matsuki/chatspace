19460309309f2598b254718e33dd643519c7f42a
Ayaki-Matsuki <super.crou@gmail.com>
2019-08-18 03:31:42 +0900

    add group-htmi,group-css
    groupのビューを追加
    もともと用意されていたgroupのビューを適切なファイルに配置し追加、hamlに変換
    group.scssをインポートするための記述を行い
    side.html.hamlのグループ編集に移行するためのlink_toの記述を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :group, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
