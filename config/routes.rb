0bc33ebe6f0757128651c5cc07edf1435440944a
Ayaki-Matsuki <super.crou@gmail.com>
2019-08-18 02:45:51 +0900

    Update routes.rb
    グループ機能の定義
    グループ機能は、新規作成機能、編集機能で構成されており、resourcesを用いて、新規作成画面を表示する:new、新規作成されたグループを保存する:create、編集画面を表示する:edit、編集されたグループを更新する:updateを、routes.rbに記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :group, only: [:new, :create, :edit, :update]
 end
