b41c8ac1f15bc24df74fb378cb7167555524d1f6
Ayaki-Matsuki <super.crou@gmail.com>
2019-08-19 23:03:08 +0900

    Display side-bar(group)
    サイドバーに作成したグループを表示させる
    groupコントローラーにindexアクションを追加し、
    新たにサイドバーのビューを追加し、リンクを変更
    _side_bar.html.hamlとして部分テンプレート化しました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  
+  def index
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -12,15 +17,26 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  
-  private 
-  def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
-  end
 
   def edit
   end
 
   def update
+
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  private 
+
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
