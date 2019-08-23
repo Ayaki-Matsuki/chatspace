9466d75e553eb342736c51e0c99fd6b7c9bb1447
Ayaki-Matsuki <super.crou@gmail.com>
2019-08-23 23:52:59 +0900

    Addshow_last_message
    サイドバーのメッセージの表示をgroupのモデルに記述
    モデルにインスタンスメソッドを定義することで、ビューの記述をシンプルにすることができるので
    show_last_messageメソッドを作り
    メッセージが投稿されている場合、されていない場合で処理を分けています。5行目で、「if (last_message = messages.last).present?」と記述することで、最新のメッセージを変数last_messageに代入しつつ、メッセージが投稿されているかどうかで場合分けを行なっています。
    
    メッセージが投稿されている場合の内部で、さらに文章が投稿されている場合、画像が投稿されている場合で処理を分けて記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,6 @@ class GroupsController < ApplicationController
   end
 
   def update
-
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
